edadb1e85f6c9cc2ff41952ae187104ade79f93a
Kevin Verhoef <1089029+kverhoef@users.noreply.github.com>
2019-12-12 08:23:46 +0100

    Update xpath.js

--- pricewatcher/amplify/backend/function/crawler/src/xpath.js
+++ pricewatcher/amplify/backend/function/crawler/src/xpath.js
@@ -2,13 +2,25 @@ module.exports = {
     findFullXpath: (root, xpathFull) => {
         const paths = xpathFull.split('/');
         let currentElement = root;
+        let parent = currentElement;
         paths.forEach((path) => {
             if (path != '') {
                 let split = path.split('[');
                 const nodeName = split[0];
                 let elementIndex = split.length > 1 ? Number.parseInt(split[1].replace(']', ''))-1 : 0;
                 const foundNodes = currentElement.childNodes.filter((n) => n.tagName === nodeName);
-                currentElement = foundNodes[elementIndex];
+
+                if (!foundNodes[elementIndex]) {
+                    // start again from parent
+                    const found = module.exports.findNodeDeep(parent, nodeName);
+                    if (found) {
+                        parent = currentElement;
+                        currentElement = found;
+                    }
+                } else {
+                    parent = currentElement;
+                    currentElement = foundNodes[elementIndex];
+                }
             }
         });
         return currentElement;
@@ -21,7 +33,24 @@ module.exports = {
                 if (searchNode.nodeType === 3) {
                     found = searchNode;
                 } else {
-                    node.childNodes.forEach((c) => f(c))
+                    searchNode.childNodes.forEach((c) => f(c))
+                }
+            }
+        };
+        f(node);
+        return found;
+    },
+
+    findNodeDeep: (node, tagName) => {
+        let found;
+        const f = (searchNode) => {
+            if (!found) {
+                if (searchNode.tagName === tagName) {
+                    found = searchNode;
+                } else {
+                    if (searchNode.childNodes) {
+                        searchNode.childNodes.filter((n) => n.nodeType !== 3).forEach((c) => f(c))
+                    }
                 }
             }
         };
@@ -30,8 +59,11 @@ module.exports = {
     },
 
     getValue: (node) => {
-        const matchedNumber = node.rawText.match('[+-]?([0-9]*[.])?[0-9]+')[0];
-        return Number.parseFloat(matchedNumber);
+        const matched = node.rawText.match('[+-]?([0-9]*[.])?[0-9]+');
+        if (matched.length) {
+            const matchedNumber = node.rawText.match('[+-]?([0-9]*[.])?[0-9]+')[0];
+            return Number.parseFloat(matchedNumber);
+        }
     }
 
 }
